fede38954c3c2a965bc5a0411e2eecec62a47cf4
Karl Heilmann <kh@karl-heilmann.info>
2024-04-04 01:20:14 +0200

    DPA-2: add component foo

---
+++ src/app/foo/foo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-foo',
+  templateUrl: './foo.component.html',
+  styleUrl: './foo.component.css'
+})
+export class FooComponent {
+
+}
